851773ebfb2be9a47c52a169bafea6d32aca7e61
taraasik <taraasik@gmail.com>
2020-02-12 13:51:24 -0500

    Adding first version of project

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,109 @@
 /* Class: CISC 3130
- * Section: MY9, TY2, TY9
- * EmplId:
- * Name: Professor Katherine Chuang
+ * Section: TY9
+ * EmplId: ****8149
+ * Name: Elena Tarasova
  */
 
- // Imports go towards top of the file
-import java.util.*;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.*;
 
-// This class handles the effort related to opening and reading the data file,
-// and parses the needed information with available methods to access data later.
-class GreatExpectations extends Thread
-{
-  // Java classes can have private members that are not accessible outside the class
-  private int chapters = 0;
+//Main class shows output data to the executive and VIP client
+public class Main {
 
-  // Java members can be final, meaning they can't be changed
-  final static String data = "../data/great_expectations.txt";
+    private static final String FILE_NAME = "data/regional-global-weekly-2020-01-17--2020-01-24.csv";
 
-  // This is a default constructor that gets called when an object is created
-  public GreatExpectations()
-  {
-    System.out.println("Constructor called. Parsing this file: " + data);
-  }
+    public static void main(String[] args) throws Exception {
+        //create Map to store name of artists and how many times they appear in chart
+        Map<String, Integer> chart = createChart(FILE_NAME);
 
-  // getter method to retrieve the private class member from another class
-  int getGreatExpectations()
-  {
-    return this.chapters;
-  }
+        //display chart by appearance artist in chart
+        displayOrderedChart(chart);
 
-  // setter method to set the private class member
-  void setGreatExpectations(int i)
-  {
-    this.chapters = i;
-  }
+        //display chart by artist name
+        displayArtistInAlphabeticalOrder(chart);
 
-  // Implementation in its own function with default access modifier to the class
-  void openBookandStudy()
-  {
-    System.out.print(" starting method openBookandStudy ");
-    File file = new File(data);
-    int counter = 0;
-
-    // try-catch block to handle exceptions when opening and reading files
-    try
-    {
-      Scanner scanner = new Scanner(file);
-
-      while (scanner.hasNextLine())
-      {
-          String s = scanner.nextLine();
-          if (s.contains("Chapter"))
-          {
-            // Increment the counter, this is the only necessary line in this if block
-            counter += 1;
-
-            // Thread sleeping is for cosmetic reasons as . is printed to screen
-            Thread.sleep(300);
-            System.out.print(".");
-          }
-      }
-      // Good habit to close the scanner after done reading a file
-      scanner.close();
-
-      // Make thread sleep, for cosmetic reasons
-      Thread.sleep(100);
-
-    } catch (Exception e) {
-      System.out.println("uh oh!");
     }
 
-    // Save the number of chapters counted to instance member so it can be accessed later
-    this.setGreatExpectations(counter);
-  }
-} // end class GreatExpectations
+    //create chart from file
+    private static Map<String, Integer> createChart(String fileName) throws Exception {
+        Scanner scanner = new Scanner(new File(fileName));
+        String description = scanner.nextLine();
+        String title = scanner.nextLine();
 
-// There can only be one public class in a Java file.
-// The public class name must match the file name.
-public class Main
-{
+        Map<String, Integer> chart = new HashMap<>();
+        while (scanner.hasNextLine()) {
+            String[] tokens = scanner.nextLine().split(",");
+            String artistName = tokens[2].trim();
 
-  // Every public class has a main method.
-  // There can only be one main method.
-  public static void main(String[] args)
-  {
-    GreatExpectations thisSemester = new GreatExpectations();
-    thisSemester.openBookandStudy();
+            //if the map contains the artistsName
+            if (chart.containsKey(artistName)) {
+                int count = chart.get(artistName);
+                chart.put(artistName, count + 1);
+            } else {
+                chart.put(artistName, 1);
+            }
+        }
+        return chart;
+    }
 
-    // Print to console w/o new line
-    System.out.print("Charles Dickens wrote ");
-    System.out.print(thisSemester.getGreatExpectations());
 
-    // Print to console w/ new line
-    System.out.println(" chapters in his book Great Expectations.");
-  }
+    //display ordered chart
+    private static void displayOrderedChart(Map<String, Integer> chart) {
+        List<ArtistRating> artistRatings = new LinkedList<>();
+        for (String artistName : chart.keySet()) {
+            artistRatings.add(new ArtistRating(artistName, chart.get(artistName)));
+        }
+
+        artistRatings.sort(new ArtistRatingComparator());
+
+        for (ArtistRating artistRating : artistRatings) {
+            System.out.printf("%-35s %d%n", artistRating.getArtistName(), artistRating.getRating());
+        }
+    }
+
+    //display artists in alphabetical order
+    public static void displayArtistInAlphabeticalOrder(Map<String, Integer> chart) {
+        //get artists names
+        Set<String> artistsNames = chart.keySet();
+
+        //sort artists names
+        TreeSet<String> sortedArtistsNames =  new TreeSet<>(artistsNames);
+
+        for (String artistName : sortedArtistsNames) {
+            System.out.printf("%-35s %d%n", artistName, chart.get(artistName));
+        }
+
+    }
+
+}
+
+class ArtistRating {
+    private String artistName;
+    private int rating;
+
+    ArtistRating(String artistName, int rating) {
+        this.artistName = artistName.trim();
+        this.rating = rating;
+    }
+
+    String getArtistName() {
+        return artistName;
+    }
+
+    int getRating() {
+        return rating;
+    }
+}
+
+class ArtistRatingComparator implements Comparator<ArtistRating> {
+    @Override
+    public int compare(ArtistRating artistRating1, ArtistRating artistRating2) {
+        // sort by rating first
+        int result = -(artistRating1.getRating() - artistRating2.getRating());
+        // then sort by name if rating is the same
+        if (result == 0) {
+            result = artistRating1.getArtistName().compareToIgnoreCase(artistRating2.getArtistName());
+        }
+        return result;
+    }
 }
